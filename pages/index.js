2b4f412344720169bcbc8b47d447d7e8e111502c
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2022-11-19 12:48:03 +0100

    -

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,18 @@ export default function Home() {
 Home.getLayout = function getLayout(page) {
   return <HomeLayout>{page}</HomeLayout>
 }
+
+
+
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+  console.log(context.query)
+  var fs = require('fs');
+
+  fs.appendFile('mynewfile1.txt', ' This is my text.\n', function (err) {
+    if (err) throw err;
+    console.log('Error!');
+});
+
+  return { props: {  } }
+}
