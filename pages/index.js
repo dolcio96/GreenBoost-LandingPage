eb6066d2a4f7440eea8acdde395825ffe2ad24a5
Giovanni Misso <giova.misso@gmail.com>
2022-11-23 20:28:27 +0100

    -

--- pages/index.js
+++ pages/index.js
@@ -24,19 +24,19 @@ Home.getLayout = function getLayout(page) {
 
 
 
-export async function getServerSideProps(context) {
-  // Fetch data from external API
-  const email = context.query.email
-  const type = context.query.type
-  if (email && type) {
-    var fs = require('fs');
+// export async function getServerSideProps(context) {
+//   // Fetch data from external API
+//   const email = context.query.email
+//   const type = context.query.type
+//   if (email && type) {
+//     var fs = require('fs');
 
-    fs.appendFile('collected_emails.csv', email + '\t' + type + '\n', function (err) {
-      if (err) throw err;
-      console.log('Error');
-    });
-  }
+//     fs.appendFile('collected_emails.csv', email + '\t' + type + '\n', function (err) {
+//       if (err) throw err;
+//       console.log('Error');
+//     });
+//   }
 
 
-  return { props: {} }
-}
+//   return { props: {} }
+// }
