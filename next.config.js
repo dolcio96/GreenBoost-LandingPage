882a57e0dcf5c46132e8c0fcf8b94b7f9f903ec7
Giovanni Misso <giova.misso@gmail.com>
2022-11-23 20:12:55 +0100

    -

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "/",
   },
-  basePath: "/test_gb",
-  assetPrefix: "/test_gb",
+  basePath: "/GreenBoost-LandingPage",
+  assetPrefix: "/GreenBoost-LandingPage",
 }
 
 module.exports = nextConfig
