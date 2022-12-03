9722817bfa91e4a22e06102c273e0994015d2330
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2022-12-03 12:41:34 +0100

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,17 @@ import { ChakraProvider } from '@chakra-ui/react'
 //import '../styles/test.scss'
 //import '../styles/test2.scss'
 import { myTheme } from 'styles/theme';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps } }) {
   const getLayout = Component.getLayout || ((page) => page)
   return <>
-      <ChakraProvider theme={myTheme}>
-        {getLayout(<Component {...pageProps} />)}
-      </ChakraProvider>
+    <ChakraProvider theme={myTheme}>
+      {getLayout(<Component {...pageProps} />)}
+      <Analytics />
+    </ChakraProvider>
   </>
 }
 
